7e66a9f09913dce31af1e66fa258c562464a7410
Øivind Jorfald <otj@itf-as.no>
2023-01-03 16:31:37 +0100

    Add Majid and workshop

--- public/js/scrolling-nav.js
+++ public/js/scrolling-nav.js
@@ -1,4 +1,15 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
+$(document).ready(function (e) {
+    // Open ws on launch
+    if (window.document.URL.includes("ws=true")) {
+        $('#day-1').addClass("active in")
+        $('#day-2').removeClass("active in")
+
+        $('#tab-day-1').addClass("active")
+        $('#tab-day-2').removeClass("active")
+    }
+});
+
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
